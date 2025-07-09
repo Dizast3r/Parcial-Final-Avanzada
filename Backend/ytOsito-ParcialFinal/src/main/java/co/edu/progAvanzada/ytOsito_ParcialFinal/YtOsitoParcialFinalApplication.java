aca6eb6cc1337746b90a483a2ce76a413429677b
Dizast3r <jm2006900@gmail.com>
2025-07-09 17:42:46 -0500

    Creando el repo

---
+++ Backend/ytOsito-ParcialFinal/src/main/java/co/edu/progAvanzada/ytOsito_ParcialFinal/YtOsitoParcialFinalApplication.java
@@ -0,0 +1,13 @@
+package co.edu.progAvanzada.ytOsito_ParcialFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtOsitoParcialFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtOsitoParcialFinalApplication.class, args);
+	}
+
+}
---
+++ Backend/ytOsito-ParcialFinal/src/test/java/co/edu/progAvanzada/ytOsito_ParcialFinal/YtOsitoParcialFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.progAvanzada.ytOsito_ParcialFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtOsitoParcialFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
